dde36c3e6d4986952bcbafcde42d70a13bed4428
Almaris Rondon <almarisrondon@gmail.com>
2024-08-21 13:51:07 -0400

    modificaciones en el index.js

--- index.js
+++ index.js
@@ -2,3 +2,11 @@ precio = 400000;
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio;
+
+const btnsumar = document.getElementById("sumar");
+
+btnsumar.addEventListener("click", () => {
+  const cantidadElement = document.querySelector(".cantidad");
+  const cantidad = Number(cantidadElement.innerHTML) + 1;
+  cantidadElement.innerHTML = cantidad;
+});
