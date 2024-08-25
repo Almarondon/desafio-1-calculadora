1c598e5277f9711cc9e1d0cdfa6cb85cdbdc7153
Almaris Rondon <almarisrondon@gmail.com>
2024-08-25 19:30:59 -0400

    se modifico el js y se agrego readme

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,20 +1,22 @@
-precio = 400000;
+const precio = 400000;
 
-precioSpan = document.querySelector(".precio-inicial");
-precioSpan.innerHTML = precio;
-
+const precioSpan = document.querySelector(".precio-inicial");
 const btnsumar = document.getElementById("sumar");
+const btnrestar = document.getElementById("restar");
+const valorTotalSpan = document.querySelector(".valor-total");
+
+precioSpan.innerHTML = precio;
 
 btnsumar.addEventListener("click", () => {
   const cantidadElement = document.querySelector(".cantidad");
   const cantidad = Number(cantidadElement.innerHTML) + 1;
   cantidadElement.innerHTML = cantidad;
+  valorTotalSpan.innerHTML = cantidad * precio;
 });
-
-const btnrestar = document.getElementById("restar");
 
 btnrestar.addEventListener("click", () => {
   const cantidadElement = document.querySelector(".cantidad");
   const cantidad = Number(cantidadElement.innerHTML) - 1;
   cantidadElement.innerHTML = cantidad;
+  valorTotalSpan.innerHTML = cantidad * precio;
 });
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # desafio-1-calculadora
+
+Proyecto de calculadora en equipo 🧮
+
+## Colaboradores
+
+| Usuario         | Nombre            |
+| --------------- | ----------------- |
+| @almarondon     | Almaris Rondon    |
+| @maximiliano121 | Maximiliano Vidal |
